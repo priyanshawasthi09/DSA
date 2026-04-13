b08d3015d2bcfcac1549f8bf021eaea94308d3a6
priyansh-awasthi-TTN <priyansh.awasthi@tothenew.com>
2026-04-13 15:54:39 +0530

    arrays

--- src/DSA/Arrays/Easy/FindMissingNumber.java
+++ src/DSA/Arrays/Easy/FindMissingNumber.java
@@ -1,5 +1,8 @@
 package DSA.Arrays.Easy;
 
+import java.util.Arrays;
+import java.util.Map;
+
 public class FindMissingNumber {
     public static void main(String[] args) {
         int[] arr = {8, 2, 4, 5, 3, 7, 1};
@@ -8,17 +11,22 @@ public class FindMissingNumber {
     }
 
     private static int missingNumber(int[] arr) {
-        int min = Integer.MAX_VALUE;
-        for(int i : arr){
-            min = Math.min(min, i);
+//        int size = arr.length + 1;
+//        int total_sum = size * (size + 1) / 2;
+//        for(int i = 0 ; i < arr.length ; i++){
+//            total_sum -= arr[i];
+//        }
+//        return total_sum;
+
+        int xor1 = 0, xor2 = 0;
+        for(int i = 0 ; i < arr.length ; i++){
+            xor1 ^= arr[i];
         }
 
-        for(int i = min ; i < arr.length ; i++){
-            for(int j = 0 ; j < arr.length ; j++){
-                if(i == arr[j]){
-
-                }
-            }
+        for(int i = 1 ; i <= arr.length + 1 ; i++){
+            xor2 ^= i;
         }
+
+        return xor1 ^ xor2;
     }
 }
